2f3970822c0a55bf7984c4001c64608a1e373ba7
Keerthana <keerthanaveldanda05@gmail.com>
2025-12-21 19:18:01 +0530

    Added

--- backend/function_app.py
+++ backend/function_app.py
@@ -2,5 +2,57 @@ import azure.functions as func
 import datetime
 import json
 import logging
+from openai import OpenAI
+import os
 
 app = func.FunctionApp()
+
+@app.route(route="chat",methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)
+def chat(req:func.HttpRequest):
+    try:
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        data=req.get_json()
+        msg=data.get("message")
+        if not msg:
+            return func.HttpResponse(
+                json.dumps({
+                    "error":"Please enter the message to respond"
+                }),
+                status_code=400,
+                mimetype="application/json"
+            )
+
+        response=client.responses.create(
+            model="gpt-5-nano",
+            input=[
+               {
+                  "role": "system",
+                   "content": "You are MindBridge, a supportive mental health assistant for students. Do not give medical diagnosis."
+               },
+               {
+                    "role": "user",
+                    "content": msg
+               }
+            ]
+        )
+        reply=response.output_text()
+        return func.HttpResponse(
+            json.dumps({
+                "reply":reply
+            }),
+            status_code=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        return func.HttpResponse(
+            json.dumps({
+               logging.error(str(e))
+            }),
+            status_code=500,
+            mimetype="application/json"
+        )
+
+        
+
+
+    
