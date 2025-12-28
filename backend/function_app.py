d7972e47987d14971cdad107661141778f66e680
Keerthana <keerthanaveldanda05@gmail.com>
2025-12-28 15:11:58 +0530

    Added

--- backend/function_app.py
+++ backend/function_app.py
@@ -4,6 +4,7 @@ import json
 import logging
 from openai import OpenAI
 import os
+from cosmos_client import get_container
 
 app = func.FunctionApp()
 
@@ -36,6 +37,11 @@ def chat(req:func.HttpRequest):
             ]
         )
         reply=response.output_text
+        container=get_container("chat_summaries")
+        container.create_item({
+            "reply":reply,
+            "createdAt":datetime.datetime.isoformat()
+        })
         return func.HttpResponse(
             json.dumps({
                 "reply":reply
@@ -43,12 +49,49 @@ def chat(req:func.HttpRequest):
             status_code=200,
             mimetype="application/json"
         )
+        
     except Exception as e:
         logging.error(str(e))
         return func.HttpResponse(
             json.dumps({
                "error":"Internal server error"
             }),
+            status_code=500,
+            mimetype="application/json"
+        )
+@app.route(route="chat",methods=["POST"],auth_level=func.AuthLevel.ANONYMOUS)
+def enter(req:func.HttpRequest)->func.HttpResponse:
+    try:
+        data=req.get_json()
+        stu_name=data.get("studentname")
+        email=data.get("email")
+        password=data.get("password")
+        if not stu_name or not email or not password:
+            return func.HttpResponse(
+                json.dumps({
+                    "error":"The above details are required to enter"
+                }),
+                status_code=400,
+                mimetype="application/json"
+            )
+        container=get_container("users")
+        container.create_item({
+            "studentName":stu_name,
+            "email":email,
+            "password":password,
+            "createdAt":datetime.datetime.isoformat()
+        })
+        return func.HttpResponse(
+            json.dumps({
+                "success":True,
+                "message":"Login Successful"
+            }),
+            status_code=200,
+            mimetype="application/json"
+        )
+    except Exception as e:
+        return func.HttpResponse(
+            json.dumps({"error": str(e)}),
             status_code=500,
             mimetype="application/json"
         )
